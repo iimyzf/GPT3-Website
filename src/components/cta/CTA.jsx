5620ee852b9a332254ec6a17a5bd99dd1901a898
iimyzf <agnaou004@gmail.com>
2023-01-29 16:45:04 +0100

    Possibility & CTA sections complete

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
 import "./cta.css";
 
 const Cta = () => {
-  return (
-	<div>Cta</div>
-  )
-}
+    return (
+        <div className="gpt3--cta">
+            <div className="gpt3--cta-content">
+                <p>Request Early Access</p>
+                <h3>
+                    Register today & start exploring the endless possiblities.
+                </h3>
+            </div>
+            <div className="gpt3--cta-button">
+                <button type="button">Get Started</button>
+            </div>
+        </div>
+    );
+};
 
-export default Cta
+export default Cta;
