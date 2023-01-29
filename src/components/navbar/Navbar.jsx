564c3482488dd60f1074599139d1e92a660682c5
iimyzf <agnaou004@gmail.com>
2023-01-29 01:42:07 +0100

    Navbar complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,39 +1,33 @@
-import React from "react";
-import "./navbar.css";
+import React, { useState } from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
-
-const Menu = () => (
-    <>
-        <p>
-            <a href="#home">Home</a>
-        </p>
-        <p>
-            <a href="#wgpt3">What is GPT3</a>
-        </p>
-        <p>
-            <a href="#possibility">Open AI</a>
-        </p>
-        <p>
-            <a href="#features">Case Studies</a>
-        </p>
-        <p>
-            <a href="#blog">Library</a>
-        </p>
-    </>
-);
+import "./navbar.css";
 
 const Navbar = () => {
-    const [toggleMenu, setToggleMenu] = React.useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
 
     return (
         <div className="gpt3--navbar">
             <div className="gpt3--navbar-links">
                 <div className="gpt3--navbar-links-logo">
-                    <img src={logo} alt="logo" />
+                    <img src={logo} />
                 </div>
                 <div className="gpt3--navbar-links-container">
-                    <Menu />
+                    <p>
+                        <a href="#home">Home</a>
+                    </p>
+                    <p>
+                        <a href="#wgpt3">What is GPT3?</a>
+                    </p>
+                    <p>
+                        <a href="#possibility">Open AI</a>
+                    </p>
+                    <p>
+                        <a href="#features">Case Studies</a>
+                    </p>
+                    <p>
+                        <a href="#blog">Library</a>
+                    </p>
                 </div>
             </div>
             <div className="gpt3--navbar-sign">
@@ -57,11 +51,25 @@ const Navbar = () => {
                 {toggleMenu && (
                     <div className="gpt3--navbar-menu-container scale-up-center">
                         <div className="gpt3--navbar-menu-container-links">
-                            <Menu />
-                            <div className="gpt3--navbar-manu-container-links-sign">
-                                <p>Sign in</p>
-                                <button type="button">Sign up</button>
-                            </div>
+                            <p>
+                                <a href="#home">Home</a>
+                            </p>
+                            <p>
+                                <a href="#wgpt3">What is GPT3?</a>
+                            </p>
+                            <p>
+                                <a href="#possibility">Open AI</a>
+                            </p>
+                            <p>
+                                <a href="#features">Case Studies</a>
+                            </p>
+                            <p>
+                                <a href="#blog">Library</a>
+                            </p>
+                        </div>
+                        <div className="gpt3--navbar-menu-container-links-sign">
+                            <p>Sign in</p>
+                            <button type="button">Sign up</button>
                         </div>
                     </div>
                 )}
